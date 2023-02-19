2111940a507e19ff652989e79628791bc187eae1
kiv1n <aloha@kiv1n.ru>
2023-02-19 10:27:29 +0300

    Add simple personal page with mui components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import type { AppProps } from "next/app";
+
+const typography = {
+  fontFamily: [
+    "Prompt",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "sans-serif",
+  ].join(","),
+};
+
+const theme = createTheme({
+  typography,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
